56fca3d17f06efe17e171c38c9bf4be5caa70bf5
FUNnyManiac <70852426+FUNnyManiac@users.noreply.github.com>
2020-10-07 20:24:47 +0300

    Complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let q = String(n);
+        q = q.split('');
+    if(q[0]==='-'){
+        let minus = q.splice(q[0], 1);
+        return q.reverse().join('')
+    } return q.reverse().join('');
 }
